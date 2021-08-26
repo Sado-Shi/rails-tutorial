00ae57e606252f63cc8c84096b83a90947aa218e
tsuchiya <tsuchiya@grabss.co.jp>
2021-08-26 09:29:04 +0900

    インデントのズレを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,8 @@ class UsersController < ApplicationController
     before_action :logged_in_user, only: [:edit, :update, :show, :index, :destroy]
   # ログインしているユーザーが自分か判定するリスト
     before_action :correct_user,   only: [:edit, :update]
-    # 管理者だけが、destroyアクションに触れるように
-      before_action :admin_user,     only: :destroy
+  # 管理者だけが、destroyアクションに触れるように
+    before_action :admin_user,     only: :destroy
 
   def index
     @users = User.paginate(page: params[:page], per_page: 10)
